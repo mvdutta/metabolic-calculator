955030a59e96c9ea697ce31770fb8be2f102a563
Maia Dutta <mermaia33@gmail.com>
2022-06-16 19:36:19 -0500

    Added JS for clear form button.

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ function calculate() {
     const data = Object.fromEntries(new FormData(form).entries())
     const {activity, age, gender, height, injury, units, weight}=data
     document.getElementById("genderSpan").innerHTML=gender;
-    document.getElementById("ageSpan").innerHTML=age;
-    document.getElementById("weightSpan").innerHTML=weight;
-    document.getElementById("heightSpan").innerHTML=height;
+    document.getElementById("ageSpan").innerHTML=`${age} years`;
+    document.getElementById("weightSpan").innerHTML=`${weight} ${units}`;
+    document.getElementById("heightSpan").innerHTML=`${height} in.`;
+    document.getElementById("activitySpan").innerHTML=activity;
+    document.getElementById("injurySpan").innerHTML=injury;
    
+}
+
+function reset() {
+    let inputs= document.getElementsByClassName("to-reset")
+    for (let input of inputs) {
+        input.value= "" 
+    }
+
 }
 
 document.getElementById("submitButton1").addEventListener("click",calculate);
